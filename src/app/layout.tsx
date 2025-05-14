1812ac3458a051e6ee895b6f4a1c0b9d79aee5c6
Juan Dillon <juancruzdillon123@gmail.com>
2025-05-14 01:21:50 +0000

    no aparecen los colores en el calendario

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         <Toaster />
       </body>
